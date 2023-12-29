3c6c6910fe65ed8b289cc0b6d1f60b1c9796e840
phonil <phonil@naver.com>
2023-12-30 02:31:27 +0900

    theme3: select sort

---
+++ algorithm/src/main/java/theme3/SelectSort.java
@@ -0,0 +1,37 @@
+package theme3;
+
+public class SelectSort {
+
+    public static void selectSort(int[] arr) {
+
+        int size = arr.length;
+        int min;
+
+        for (int i = 0; i < size - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[min] > arr[j])
+                    min = j;
+            }
+            swap(arr, i, min);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int min) {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {7, 3, 6, 1, 5, 4, 13, 11};
+        selectSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+        }
+
+    }
+}
