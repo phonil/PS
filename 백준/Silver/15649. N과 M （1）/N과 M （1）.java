fcbaa9bfe1bf741ecb8e19068efad0f46d1ba841
강승우 <112797234+phonil@users.noreply.github.com>
2024-04-06 15:33:17 +0900

    [Silver III] Title: N과 M (1), Time: 2608 ms, Memory: 65060 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+// N과 M (1)
+public class Main {
+
+    public static int[] arr;
+    public static boolean[] isUsed;
+
+    public static int N;
+    public static int M;
+
+    public static void func(int k) {
+        if (k == M) { // base condition
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (!isUsed[i]) {
+                arr[k] = i;
+                isUsed[i] = true;
+                func(k + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[M];
+        isUsed = new boolean[N + 1];
+
+        func(0);
+    }
+}
