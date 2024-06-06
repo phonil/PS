d36d133cd42fb17b5bf1d5ad52d81be02902aa25
강승우 <112797234+phonil@users.noreply.github.com>
2024-06-06 22:45:06 +0900

    [Gold V] Title: 괄호의 값, Time: 132 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -1,19 +1,18 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        int sum = 0; // 괄호 안 덧셈 시 사용
+        int num = 1; // 곱셈 시 사용
         Stack<Character> stack = new Stack<>();
-
-        int num = 1;
-        int sum = 0;
-
-        // 구현
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(') {
@@ -23,7 +22,7 @@ public class Main {
                 stack.push(c);
                 num *= 3;
             } else if (c == ')') {
-                if (stack.isEmpty() || stack.peek() != '(') { // stack.peek() : 이제 뽑을 거
+                if (stack.isEmpty() || stack.peek() != '(') {
                     sum = 0;
                     break;
                 }
@@ -31,8 +30,8 @@ public class Main {
                     sum += num;
                 stack.pop();
                 num /= 2;
-            } else {
-                if (stack.isEmpty() || stack.peek() != '[') { // stack.peek() : 이제 뽑을 거
+            } else { // if c == ']'
+                if (stack.isEmpty() || stack.peek() != '[') {
                     sum = 0;
                     break;
                 }
@@ -46,6 +45,5 @@ public class Main {
             System.out.println("0");
         else
             System.out.println(sum);
-
     }
 }
