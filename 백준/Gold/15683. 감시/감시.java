768afe27047b80346747a42c0a0a8a315f346888
강승우 <112797234+phonil@users.noreply.github.com>
2024-10-09 12:34:19 +0900

    [Gold III] Title: 감시, Time: 364 ms, Memory: 28792 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,15 +9,15 @@ public class Main {
 
     public static int N, M;
     public static int[][] board1, board2;
-    public static int[] dx = {1, 0, -1, 0};
-    public static int[] dy = {0, 1, 0, -1};
     public static List<Pair> cctv = new ArrayList<>();
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, 1, 0, -1};
 
     public static boolean isWall(int x, int y) {
         return x < 0 || y < 0 || x >= N || y >= M;
     }
 
-    public static void dirCheck(int x, int y, int dir) {
+    public static void checkBoard(int x, int y, int dir) {
         dir %= 4;
         while (true) {
             x += dx[dir];
@@ -27,7 +26,6 @@ public class Main {
             if (board2[x][y] != 0) continue;
             board2[x][y] = 7;
         }
-
     }
 
     public static void main(String[] args) throws IOException {
@@ -43,9 +41,9 @@ public class Main {
         }
         int mn = 0;
         for (int i = 0; i < N; i++) {
-            String[] line = br.readLine().split(" ");
+            String[] num = br.readLine().split(" ");
             for (int j = 0; j < M; j++) {
-                board1[i][j] = Integer.parseInt(line[j]);
+                board1[i][j] = Integer.parseInt(num[j]);
                 if (board1[i][j] != 0 && board1[i][j] != 6)
                     cctv.add(new Pair(i, j));
                 if (board1[i][j] == 0)
@@ -56,47 +54,49 @@ public class Main {
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < M; j++)
                     board2[i][j] = board1[i][j];
+
             int brute = tmp;
             for (int i = 0; i < cctv.size(); i++) {
                 int dir = brute % 4;
                 brute /= 4;
                 int x = cctv.get(i).x;
                 int y = cctv.get(i).y;
-                if (board1[x][y] == 1)
-                    dirCheck(x, y, dir);
-                else if (board1[x][y] == 2) {
-                    dirCheck(x, y, dir);
-                    dirCheck(x, y, dir + 2);
-                } else if (board1[x][y] == 3) {
-                    dirCheck(x, y, dir);
-                    dirCheck(x, y, dir + 1);
-                } else if (board1[x][y] == 4) {
-                    dirCheck(x, y, dir);
-                    dirCheck(x, y, dir + 1);
-                    dirCheck(x, y, dir + 2);
+                int type = board1[x][y];
+                if (type == 1) {
+                    checkBoard(x, y, dir);
+                } else if (type == 2) {
+                    checkBoard(x, y, dir);
+                    checkBoard(x, y, dir + 2);
+                } else if (type == 3) {
+                    checkBoard(x, y, dir);
+                    checkBoard(x, y, dir + 1);
+                } else if (type == 4) {
+                    checkBoard(x, y, dir);
+                    checkBoard(x, y, dir + 1);
+                    checkBoard(x, y, dir + 2);
                 } else {
-                    dirCheck(x, y, dir);
-                    dirCheck(x, y, dir + 1);
-                    dirCheck(x, y, dir + 2);
-                    dirCheck(x, y, dir + 3);
+                    checkBoard(x, y, dir);
+                    checkBoard(x, y, dir + 1);
+                    checkBoard(x, y, dir + 2);
+                    checkBoard(x, y, dir + 3);
                 }
             }
             int val = 0;
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < M; j++)
-                    if (board2[i][j] == 0) val++;
-            mn = Math.min(val, mn);
+                    if (board2[i][j] == 0)
+                        val++;
+
+            mn = Math.min(mn, val);
         }
-        System.out.print(mn);
+        System.out.println(mn);
     }
 }
 
 class Pair {
     int x, y;
-
     public Pair(int x, int y) {
         this.x = x;
         this.y = y;
     }
 }
-
