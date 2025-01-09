2a23c2f4d2a50578a428a52016b8c671c2a0f962
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-09 18:00:10 +0900

    [Silver I] Title: 곱셈, Time: 108 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,27 +1,26 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static Long modRecursion(Long A, Long B, Long C) {
-        if (B == 1)
-            return A % C;
-
-        Long val = modRecursion(A, B/2, C);
-        val = val * val % C;
-
-        if (B % 2 == 0)
-            return val;
-        return val * A % C;
+    static long pow(long a, long b, long m) {
+        if (b == 1)
+            return a % m;
+        long val = pow(a, b/2, m);
+        val = val * val % m;
+        if (b % 2 == 0) return val;
+        return val * a % m;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        long A = Long.parseLong(split[0]);
+        long B = Long.parseLong(split[1]);
+        long C = Long.parseLong(split[2]);
 
-        Long A = sc.nextLong();
-        Long B = sc.nextLong();
-        Long C = sc.nextLong();
-
-        System.out.println(modRecursion(A, B, C));
+        System.out.println(pow(A, B, C));
     }
 }
