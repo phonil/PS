39a2a1f80641a84b01fbe600a757ef7695a19694
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-10 18:16:20 +0900

    [Silver II] Title: 색종이 만들기, Time: 136 ms, Memory: 16576 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,59 +1,51 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-// 색종이 만들기
 public class Main {
+    static int[][] board;
+    static int white = 0;
+    static int blue = 0;
 
-    public static int[][] paper;
-    public static int[] ans = new int[2]; // 0, 1
-
-    public static void recursion(int r, int c, int N) {
-        if (check(r, c, N)) {
-            if (paper[r][c] == 0)
-                ans[0]++;
-            else
-                ans[1]++;
-
+    public static void func(int r, int c, int n) {
+        if (check(r, c, n)) {
+            int startNum = board[r][c];
+            if (startNum == 0) white++;
+            else blue++;
             return;
         }
-
-        int newSize = N / 2;
-
-//        recursion(r, c, newSize);
-//        recursion(r, c + newSize, newSize);
-//        recursion(r + newSize, c, newSize);
-//        recursion(r + newSize, c + newSize, newSize);
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                recursion(r + i * newSize, c + j * newSize, newSize);
-            }
-        }
+        int bound = n / 2;
+        func(r, c, bound);
+        func(r, c + bound, bound);
+        func(r + bound, c, bound);
+        func(r + bound, c + bound, bound);
     }
 
-    public static boolean check(int r, int c, int N) {
-        int num = paper[r][c];
-
-        for (int i = r; i < r + N; i++) {
-            for (int j = c; j < c + N; j++) {
-                if (num != paper[i][j])
-                    return false;
+    public static boolean check(int r, int c, int n) {
+        if (n == 1) {
+            return true;
+        }
+        for (int i = r; i < r + n; i++) {
+            for (int j = c; j < c + n; j++) {
+                if (board[r][c] != board[i][j]) return false;
             }
         }
         return true;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        paper = new int[N][N];
-
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                paper[i][j] = sc.nextInt();
-
-        recursion(0, 0, N);
-        System.out.println(ans[0]);
-        System.out.println(ans[1]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            String[] split = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(split[j]);
+            }
+        }
+        func(0, 0, N);
+        System.out.println(white);
+        System.out.println(blue);
     }
 }
