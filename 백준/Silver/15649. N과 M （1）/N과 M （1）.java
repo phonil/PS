7b81d4d1974ebe5fd6a4a697cc95fb14998acc47
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-12 00:22:15 +0900

    [Silver III] Title: N과 M (1), Time: 248 ms, Memory: 22532 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,39 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static int[] arr;
-    public static boolean[] isUsed;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static boolean[] visit;
 
-    public static int N;
-    public static int M;
-
-    public static void func(int k) {
-
-        // base condition
-        if (k == M) {
-            for (int i = 0; i < M; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
+    public static void func(int m) {
+        if (m == M) {
+            for (int i : arr)
+                sb.append(i).append(" ");
+            sb.append('\n');
             return;
         }
-
         for (int i = 1; i <= N; i++) {
-            if (!isUsed[i]) {
-                arr[k] = i;
-                isUsed[i] = true;
-                func(k + 1);
-                isUsed[i] = false;
-            }
+            if (visit[i]) continue;
+            visit[i] = true;
+            arr[m] = i;
+            func(m + 1);
+            visit[i] = false;
         }
-
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        N = Integer.parseInt(split[0]);
+        M = Integer.parseInt(split[1]);
         arr = new int[M];
-        isUsed = new boolean[N + 1];
-
+        visit = new boolean[N + 1];
         func(0);
+        System.out.print(sb);
     }
-
 }
