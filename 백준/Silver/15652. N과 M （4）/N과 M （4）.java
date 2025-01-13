66149896a049f90e100bc697ed92af486bf62e56
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-14 00:07:32 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 17800 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,40 +1,37 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-// N과 M (4)
 public class Main {
 
-    private static int N, M;
-    private static int[] arr;
-    private static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
 
-    private static void func(int k) {
-        if (k == M) {
-            for (int i = 0; i < M; i++) {
-                sb.append(arr[i] + " ");
-            }
-            sb.append("\n");
+    public static void func(int cur) {
+        if (cur == M) {
+            for (int i : arr)
+                sb.append(i).append(" ");
+            sb.append('\n');
             return;
         }
         int start = 1;
-        if (k != 0)
-            start = arr[k - 1];
-
+        if (cur != 0)
+            start = arr[cur - 1];
         for (int i = start; i <= N; i++) {
-            arr[k] = i;
-            func(k + 1);
+            arr[cur] = i;
+            func(cur + 1);
         }
-
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        N = Integer.parseInt(split[0]);
+        M = Integer.parseInt(split[1]);
         arr = new int[M];
-
         func(0);
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
