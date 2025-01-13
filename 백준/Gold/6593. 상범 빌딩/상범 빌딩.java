9bb8a37d83b9d569811369301859859258c410b2
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-13 18:39:12 +0900

    [Gold V] Title: 상범 빌딩, Time: 168 ms, Memory: 18568 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -1,82 +1,73 @@
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] dx = {-1, 1, 0, 0, 0, 0};
-        int[] dy = {0, 0, -1, 1, 0, 0};
-        int[] dz = {0, 0, 0, 0, -1, 1};
+    static int L, R, C;
+    static char[][][] board;
+    static int[][][] dist;
+    static int[] dx = {-1, 0, 1, 0, 0, 0};
+    static int[] dy = {0, 1, 0, -1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
 
+    public static boolean bound(int x, int y, int z) {
+        return x < 0 || y < 0 || z < 0 || x >= R || y >= C || z >= L;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
-            int L = sc.nextInt(); // 층
-            int R = sc.nextInt(); // 행
-            int C = sc.nextInt(); // 열
-            sc.nextLine();
+            String[] split = br.readLine().split(" ");
+            L = Integer.parseInt(split[0]);
+            R = Integer.parseInt(split[1]);
+            C = Integer.parseInt(split[2]);
             if (L == 0 && R == 0 && C == 0)
                 break;
-            char[][][] map = new char[R][C][L];
-            int[][][] dist = new int[R][C][L];
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < C; j++) {
-                    Arrays.fill(dist[i][j], -1);
-                }
-            }
-            // map 채우기
-            int l = L;
-            Queue<Pair> queue = new LinkedList<>();
-            while (l-- > 0) {
-                for (int i = 0; i < R; i++) {
-                    String s = sc.nextLine();
-                    for (int j = 0; j < C; j++) {
-                        char a = s.charAt(j);
-                        map[i][j][l] = a;
-                        if (a == 'S') {
-                            queue.offer(new Pair(i, j, l));
-                            dist[i][j][l] = 0;
-                        }
+            // init
+            board = new char[R][C][L];
+            dist = new int[R][C][L];
+            Queue<Pair> q = new LinkedList<>();
+            for (int i = 0; i < L; i++) {
+                for (int j = 0; j < R; j++) {
+                    String line = br.readLine();
+                    for (int k = 0; k < C; k++) {
+                        char c = line.charAt(k);
+                        board[j][k][i] = c;
+                        if (c == '.') dist[j][k][i] = -1;
+                        if (c == 'S') q.offer(new Pair(j, k, i));
                     }
                 }
-                sc.nextLine();
+                br.readLine();
             }
-            int minute = 0;
             // BFS
-            while (!queue.isEmpty()) {
-                Pair now = queue.poll();
-                if (map[now.x][now.y][now.z] == 'E') {
-                    minute = dist[now.x][now.y][now.z];
-                    break;
-                }
+            while (!q.isEmpty()) {
+                Pair cur = q.poll();
                 for (int i = 0; i < 6; i++) {
-                    int nx = now.x + dx[i];
-                    int ny = now.y + dy[i];
-                    int nz = now.z + dz[i];
-
-                    if (nx < 0 || ny < 0 || nz < 0 || nx >= R || ny >= C || nz >= L)
-                        continue;
-                    if (dist[nx][ny][nz] != -1)
-                        continue;
-                    if (map[nx][ny][nz] == '#')
-                        continue;
-
-                    queue.offer(new Pair(nx, ny, nz));
-                    dist[nx][ny][nz] = dist[now.x][now.y][now.z] + 1;
-
+                    int nx = cur.x + dx[i];
+                    int ny = cur.y + dy[i];
+                    int nz = cur.z + dz[i];
+                    if (bound(nx, ny, nz) || board[nx][ny][nz] == '#') continue;
+                    if (board[nx][ny][nz] != 'E' && dist[nx][ny][nz] != -1) continue;
+                    q.offer(new Pair(nx, ny, nz));
+                    dist[nx][ny][nz] = dist[cur.x][cur.y][cur.z] + 1;
                 }
             }
-
-            if (minute == 0)
-                System.out.println("Trapped!");
+            Pair exit = null;
+            for (int i = 0; i < L; i++)
+                for (int j = 0; j < R; j++)
+                    for (int k = 0; k < C; k++)
+                        if (board[j][k][i] == 'E') exit = new Pair(j, k, i);
+            if (dist[exit.x][exit.y][exit.z] != 0)
+                System.out.println("Escaped in " + dist[exit.x][exit.y][exit.z] + " minute(s).");
             else
-                System.out.println("Escaped in " + minute + " minute(s).");
-
+                System.out.println("Trapped!");
         }
-
     }
+
     static class Pair {
         int x, y, z;
         public Pair(int x, int y, int z) {
