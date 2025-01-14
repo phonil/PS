ea438b4f9931fe96cdd990385cad192c255fe6cd
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-14 18:05:18 +0900

    [Silver II] Title: N과 M (9), Time: 188 ms, Memory: 21696 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -6,26 +6,26 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static int N, M;
-    public static int[] numArr;
-    public static int[] arr;
-    public static boolean[] isUsed;
-    public static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr, nums;
+    static boolean[] isUsed;
+    static StringBuilder sb = new StringBuilder();
 
-    public static void func(int k) {
-        if (k == M) {
+    static void func(int cur) {
+        if (cur == M) {
             for (int i : arr)
-                sb.append(i + " ");
-            sb.append("\n");
+                sb.append(i).append(" ");
+            sb.append('\n');
             return;
         }
         int tmp = 0;
         for (int i = 0; i < N; i++) {
-            if (isUsed[i] || tmp == numArr[i]) continue;
-            arr[k] = numArr[i];
-            tmp = arr[k];
+            if (isUsed[i]) continue;
+            if (tmp == nums[i]) continue;
+            arr[cur] = nums[i];
             isUsed[i] = true;
-            func(k + 1);
+            tmp = nums[i];
+            func(cur + 1);
             isUsed[i] = false;
         }
     }
@@ -36,12 +36,12 @@ public class Main {
         N = Integer.parseInt(split[0]);
         M = Integer.parseInt(split[1]);
         arr = new int[M];
-        numArr = new int[N];
+        nums = new int[N];
         isUsed = new boolean[N];
-        String[] nums = br.readLine().split(" ");
+        String[] line = br.readLine().split(" ");
         for (int i = 0; i < N; i++)
-            numArr[i] = Integer.parseInt(nums[i]);
-        Arrays.sort(numArr);
+            nums[i] = Integer.parseInt(line[i]);
+        Arrays.sort(nums);
         func(0);
         System.out.print(sb);
     }
