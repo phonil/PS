3f8e7fda1e64db3d130582ffa4dc4f370122d882
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-14 11:26:46 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,52 +1,49 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
-// N과 M (6)
 public class Main {
 
-    private static int N, M;
-    private static int[] nums; // 배열 입력 받는지
-    private static int[] arr;
-    private static boolean[] isUsed; // 내꺼 중복
-    private static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] arr, nums;
+    static boolean[] isUsed;
+    static StringBuilder sb = new StringBuilder();
 
-    private static void func(int k) {
-        if (k == M) {
+    static void func(int cur) {
+        if (cur == M) {
             for (int i = 0; i < M; i++)
-                sb.append(nums[arr[i]] + " ");
-            sb.append("\n");
+                sb.append(nums[arr[i]]).append(" ");
+            sb.append('\n');
             return;
         }
-
-        int start = k;
-        if (k != 0)
-            start = arr[k - 1] + 1;
-
-        for (int i = start; i < N; i++) {
-            if (!isUsed[i]) {
-                arr[k] = i;
-                isUsed[i] = true;
-                func(k + 1);
-                isUsed[i] = false;
-            }
+        int startIdx = 0;
+        if (cur != 0)
+            startIdx = arr[cur - 1];
+        for (int i = startIdx; i < N; i++) { // arr에 idx를 넣을 것
+            if (isUsed[i]) continue;
+            arr[cur] = i;
+            isUsed[i] = true;
+            func(cur + 1);
+            isUsed[i] = false;
         }
-
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        N = Integer.parseInt(split[0]);
+        M = Integer.parseInt(split[1]);
         arr = new int[M];
         nums = new int[N];
         isUsed = new boolean[N];
-
+        String[] line = br.readLine().split(" ");
         for (int i = 0; i < N; i++)
-            nums[i] = sc.nextInt();
+            nums[i] = Integer.parseInt(line[i]);
         Arrays.sort(nums);
         func(0);
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
