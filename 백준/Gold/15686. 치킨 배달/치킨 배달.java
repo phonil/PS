7a0f3f0c9917ab19e1868b42baee963bdc57d4bb
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-15 18:55:38 +0900

    [Gold V] Title: 치킨 배달, Time: 204 ms, Memory: 18008 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,45 +1,50 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
-    public static int N, M;
-    public static List<Pair> chicken, house;
-    public static int[][] city;
-    public static boolean[] visited;
-    public static int response = Integer.MAX_VALUE;
+    static int N, M;
+    static int[][] city;
+    static ArrayList<Pair> house = new ArrayList<>();
+    static ArrayList<Pair> chicken = new ArrayList<>();
+    static int[] arr;
+    static boolean[] visit;
+    static int mn = Integer.MAX_VALUE;
 
-    public static void func(int start, int cnt) {
-        if (cnt == M) {
+    static void func(int cur) {
+        if (cur == M) {
             int cityDist = 0;
-            for (Pair home : house) {
-                int chickDist = Integer.MAX_VALUE;
-                for (int i = 0; i < chicken.size(); i++) {
-                    if (!visited[i]) continue;
-                    Pair chick = chicken.get(i);
-                    int dist = Math.abs(home.x - chick.x) + Math.abs(home.y - chick.y);
-                    chickDist = Math.min(chickDist, dist);
+            for (int i = 0; i < house.size(); i++) {
+                Pair home = house.get(i);
+                int dist = Integer.MAX_VALUE;
+                for (int chickIdx : arr) {
+                    Pair chick = chicken.get(chickIdx);
+                    dist = Math.min(Math.abs(home.x - chick.x) + Math.abs(home.y - chick.y), dist);
                 }
-                cityDist += chickDist;
+                cityDist += dist;
             }
-            response = Math.min(response, cityDist);
+            mn = Math.min(cityDist, mn);
             return;
         }
-        for (int i = start; i < chicken.size(); i++) {
-            visited[i] = true;
-            func(i + 1, cnt + 1);
-            visited[i] = false;
+
+        int startIdx = 0;
+        if (cur != 0)
+            startIdx = arr[cur - 1];
+        for (int i = startIdx; i < chicken.size(); i++) {
+            if (visit[i]) continue;
+            arr[cur] = i;
+            visit[i] = true;
+            func(cur + 1);
+            visit[i] = false;
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] split = br.readLine().split(" ");
-        chicken = new ArrayList<>();
-        house = new ArrayList<>();
         N = Integer.parseInt(split[0]);
         M = Integer.parseInt(split[1]);
         city = new int[N][N];
@@ -48,21 +53,23 @@ public class Main {
             for (int j = 0; j < N; j++) {
                 int num = Integer.parseInt(line[j]);
                 city[i][j] = num;
-                if (num == 0) continue;
-                else if (num == 1) house.add(new Pair(i, j));
-                else if (num == 2) chicken.add(new Pair(i, j));
+                if (num == 1)
+                    house.add(new Pair(i, j));
+                else if (num == 2)
+                    chicken.add(new Pair(i, j));
             }
         }
-        visited = new boolean[chicken.size()];
-        func(0, 0);
-        System.out.print(response);
+        arr = new int[M];
+        visit = new boolean[chicken.size()];
+        func(0);
+        System.out.print(mn);
     }
-}
 
-class Pair {
-    int x,y;
-    public Pair(int x,int y) {
-        this.x=x;
-        this.y=y;
+    static class Pair {
+        int x, y;
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
