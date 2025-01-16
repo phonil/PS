a83dae31bc3661ce6d569d9d754177ad19145ab9
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-16 21:08:17 +0900

    [Silver II] Title: N과 M (11), Time: 360 ms, Memory: 34372 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,43 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr, nums;
+    static StringBuilder sb = new StringBuilder();
+
+    static void func(int cur) {
+        if (cur == M) {
+            for (int i : arr)
+                sb.append(nums[i]).append(" ");
+            sb.append('\n');
+            return;
+        }
+        int tmp = 0;
+        for (int i = 0; i < N; i++) {
+            if (tmp == nums[i]) continue;
+            arr[cur] = i;
+            tmp = nums[i];
+            func(cur + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        N = Integer.parseInt(split[0]);
+        M = Integer.parseInt(split[1]);
+        arr = new int[M];
+        nums = new int[N];
+        String[] line = br.readLine().split(" ");
+        for (int i = 0; i < N; i++)
+            nums[i] = Integer.parseInt(line[i]);
+        Arrays.sort(nums);
+        func(0);
+        System.out.print(sb);
+    }
+}
