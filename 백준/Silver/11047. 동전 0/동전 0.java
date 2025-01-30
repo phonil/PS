3bc6f9cd9cc0de8c4a87b0d5d82aa210185e7ae9
강승우 <112797234+phonil@users.noreply.github.com>
2025-01-30 20:14:46 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int N = Integer.parseInt(split[0]);
+        int K = Integer.parseInt(split[1]);
+        int[] V = new int[N];
+        for (int i = 0; i < N; i++)
+            V[i] = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            cnt += K / V[i];
+            K %= V[i];
+        }
+        System.out.print(cnt);
+    }
+}
