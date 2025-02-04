3f7ef7b54befdc2e735c3ffa9707db61bad3bfdf
강승우 <112797234+phonil@users.noreply.github.com>
2025-02-04 22:24:45 +0900

    [Silver IV] Title: 보물, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -11,17 +11,17 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] A = new int[N];
         int[] B = new int[N];
-        String[] lineA = br.readLine().split(" ");
-        String[] lineB = br.readLine().split(" ");
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(lineA[i]);
-            B[i] = Integer.parseInt(lineB[i]);
-        }
+        String[] line = br.readLine().split(" ");
+        for (int i = 0; i < N; i++)
+            A[i] = Integer.parseInt(line[i]);
         Arrays.sort(A);
+        line = br.readLine().split(" ");
+        for (int i = 0; i < N; i++)
+            B[i] = Integer.parseInt(line[i]);
         Arrays.sort(B);
         int S = 0;
         for (int i = 0; i < N; i++)
-            S += A[i] * B[N - 1 - i];
+            S += A[i] * B[N-1-i];
         System.out.print(S);
     }
 }
