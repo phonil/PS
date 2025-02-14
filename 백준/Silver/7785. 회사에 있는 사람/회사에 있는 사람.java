7585a05f5be43ea60251d2e09f3c826a9cd0266a
강승우 <112797234+phonil@users.noreply.github.com>
2025-02-15 02:10:04 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1288 ms, Memory: 64288 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            String[] split = br.readLine().split(" ");
+            if (split[1].equals("enter")) set.add(split[0]);
+            else set.remove(split[0]);
+        }
+        set.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+    }
+}
