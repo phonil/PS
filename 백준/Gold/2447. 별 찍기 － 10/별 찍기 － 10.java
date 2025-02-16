c8981ae9e57856c0d65813e7321dcdb7e0e499a2
강승우 <112797234+phonil@users.noreply.github.com>
2025-02-16 15:07:10 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 296 ms, Memory: 48252 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -5,32 +5,30 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static StringBuilder sb = new StringBuilder();
+    static int N;
     static char[][] board;
-    static void func(int r, int c, int n) {
-        if (n == 1) {
+    static StringBuilder sb = new StringBuilder();
+
+    static void func(int size, int r, int c) {
+        if (size == 1) {
             board[r][c] = '*';
             return;
         }
-        int bound = n / 3;
+        int newSize = size / 3;
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (i == 1 && j == 1) continue;
-
-                func(r + i * bound, c + j * bound, bound);
+                func(newSize, r + i * newSize, c + j * newSize);
             }
         }
     }
 
-    static boolean check(int r, int c, int n) {
-        return true;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
         board = new char[N][N];
-        func(0, 0, N);
+        func(N, 0, 0);
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (board[i][j] != '*') board[i][j] = ' ';
