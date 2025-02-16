6ce784e2ec3f4e009f4e747ee3bb4a21d719b78c
강승우 <112797234+phonil@users.noreply.github.com>
2025-02-16 23:22:04 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 15360 ms, Memory: 100684 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -5,22 +5,18 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static int N;
-    static int[] freq = new int[2000001];
-    static final int HALF = 1000000;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            freq[num + HALF]++;
-        }
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[2000001];
+        int HALF = 1000000;
+        for (int i = 0; i < N; i++)
+            arr[Integer.parseInt(br.readLine()) + HALF]++;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < freq.length; i++) {
-            while (freq[i] != 0) {
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] != 0) {
                 sb.append(i - HALF).append('\n');
-                freq[i]--;
+                arr[i]--;
             }
         }
         System.out.print(sb);
