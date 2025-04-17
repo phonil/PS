061769aa3112decb61fdd980ff4a72f2a5633f4e
강승우 <112797234+phonil@users.noreply.github.com>
2025-04-17 13:45:52 +0900

    [Gold V] Title: 탑, Time: 812 ms, Memory: 93512 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -3,44 +3,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // stack.peek > top -> ans : stack.peek.index
-        // stack.peek < top -> stack.pop -> ...
-        // stack.isEmpty -> ans : 0
-
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> index = new Stack<>();
-        for (int i = 1; i <= N; i++) {
-            int top = Integer.parseInt(st.nextToken());
-            while (true) {
-                if (stack.isEmpty()) {
-                    System.out.print("0 ");
-                    stack.push(top);
-                    index.push(i);
-                    break;
-                } else {
-                    if (stack.peek() > top) {
-                        System.out.print(index.peek() + " ");
-                        stack.push(top);
-                        index.push(i);
-                        break;
-                    } else {
-                        stack.pop();
-                        index.pop();
-                    }
-                }
-            }
-
+        String[] split = br.readLine().split(" ");
+        int[] arr = new int[N];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(split[i]);
+            arr[i] = num;
         }
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int num = arr[i];
+            while (!st.isEmpty() && arr[st.peek()] < num)
+                st.pop();
+            if (st.isEmpty()) sb.append(0).append(" ");
+            else sb.append(st.peek() + 1).append(" ");
+            st.push(i);
+        }
+        System.out.print(sb);
     }
 }
