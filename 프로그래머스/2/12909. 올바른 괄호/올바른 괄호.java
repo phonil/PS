84b74986ac35c211e8f0219fc5967822eac2ea4f
강승우 <112797234+phonil@users.noreply.github.com>
2025-04-25 16:37:34 +0900

    [level 2] Title: 올바른 괄호, Time: 26.68 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(')
+                st.push(c);
+            else {
+                if (st.isEmpty() || st.peek() == ')') answer = false;
+                else st.pop();
+            }            
+        }
+        if (!st.isEmpty()) answer = false;
+        return answer;
+    }
+}
