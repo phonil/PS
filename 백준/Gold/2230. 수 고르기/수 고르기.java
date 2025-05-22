626ca37395fbdc521a5edfbc95b84ce3d97b1cca
강승우 <112797234+phonil@users.noreply.github.com>
2025-05-22 15:10:51 +0900

    [Gold V] Title: 수 고르기, Time: 320 ms, Memory: 27576 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -7,25 +7,25 @@ import java.util.Arrays;
 public class Main {
 
     static int N, M;
-    static int[] A;
-    static int mn = Integer.MAX_VALUE;  // 초기 최소값 설정
+    static int[] arr;
+    static int mn = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] split = br.readLine().split(" ");
         N = Integer.parseInt(split[0]);
         M = Integer.parseInt(split[1]);
-        A = new int[N];
+        arr = new int[N];
         for (int i = 0; i < N; i++)
-            A[i] = Integer.parseInt(br.readLine());
-
-        Arrays.sort(A);
+            arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
         int en = 0;
         for (int st = 0; st < N; st++) {
-            while (en < N && A[en] - A[st] < M) en++;
+            while (en < N && arr[en] - arr[st] < M)
+                en++;
             if (en == N) break;
-            mn = Math.min(mn, A[en] - A[st]);
+            mn = Math.min(mn, arr[en] - arr[st]);
         }
-        System.out.print(mn);  // 결과 출력
+        System.out.print(mn);
     }
 }
