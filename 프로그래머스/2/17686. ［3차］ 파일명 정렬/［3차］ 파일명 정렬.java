3ac4b28a5f232e10b1eebb7b5efc9ef0012238c1
강승우 <112797234+phonil@users.noreply.github.com>
2025-11-19 17:43:05 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 69.65 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -2,21 +2,12 @@ import java.util.*;
 
 class Solution {
     
-    static String parse_head(String file) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < file.length(); i++) {
-            char c = file.charAt(i);
-            if(Character.isDigit(c)) break;
-            else sb.append(c);
-        }
-        return sb.toString();
-    }
-    
-    static String parse_number(String file, int startIdx) {
+    static String parse(String file, int startIdx) {
         StringBuilder sb = new StringBuilder();
         for(int i = startIdx; i < file.length(); i++) {
             char c = file.charAt(i);
-            if(!Character.isDigit(c)) break;
+            if(startIdx == 0 && Character.isDigit(c)) break;
+            if(startIdx != 0 && !Character.isDigit(c)) break;
             else sb.append(c);
         }
         return sb.toString();
@@ -30,8 +21,8 @@ class Solution {
         for(int i = 0; i < len; i++) {
             // parsing 후 names add
             String file = files[i];
-            String head = parse_head(file);
-            String number = parse_number(file, head.length());
+            String head = parse(file, 0);
+            String number = parse(file, head.length());
             String tail = file.substring(head.length() + number.length());
             names[i] = new Name(head, number, i, tail);
         }
@@ -58,100 +49,4 @@ class Solution {
             this.tail = tail;
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//     static FileName[] fileNames;
-    
-//     static void parse(String[] files) {
-//         fileNames = new FileName[files.length];
-    
-//         for(int fileIdx = 0; fileIdx < files.length; fileIdx++) {
-//             String file = files[fileIdx];
-//             int idx = 0;
-            
-//             // Head 파싱
-//             StringBuilder sb = new StringBuilder();
-//             for(int i = idx; i < file.length(); i++) {
-//                 char c = file.charAt(i);
-//                 idx = i;
-//                 if(!Character.isDigit(c)) sb.append(c);
-//                 else break;
-//             }
-//             String head = sb.toString();
-            
-//             // Number 파싱
-//             sb = new StringBuilder();
-//             boolean last = false;
-//             for(int i = idx; i < file.length(); i++) {
-//                 char c = file.charAt(i);
-//                 idx = i;
-//                 if(sb.length() < 5 && Character.isDigit(c)) sb.append(c);
-//                 else break;
-//                 if(idx == file.length() - 1) last = true;
-//             }
-//             String number = sb.toString();
-            
-//             if(last) idx++;
-                
-//             // Tail 파싱
-//             sb = new StringBuilder();
-//             for(int i = idx; i < file.length(); i++) {
-//                 char c = file.charAt(i);
-//                 sb.append(c);
-//             }
-//             String tail = sb.toString();
-            
-//             fileNames[fileIdx] = new FileName(head, number, tail);
-//         }
-//     }
-    
-//     public String[] solution(String[] files) {
-//         String[] answer = new String[files.length];
-//         parse(files);
-//         Arrays.sort(fileNames,
-//                    Comparator.comparing((FileName fn) -> fn.head.toUpperCase())
-//                     .thenComparingInt(fn -> Integer.parseInt(fn.number))
-//                    );
-        
-//         for(int i = 0; i < files.length; i++) {
-//             StringBuilder sb = new StringBuilder();
-//             FileName fn = fileNames[i];
-//             String head = fn.head;
-//             String number = fn.number;
-//             String tail = fn.tail;
-//             sb.append(head).append(number).append(tail);
-//             answer[i] = sb.toString();
-//         }
-//         return answer;
-//     }
-    
-//     static class FileName {
-//         String head, number, tail;
-//         public FileName(String head, String number, String tail) {
-//             this.head = head;
-//             this.number = number;
-//             this.tail = tail;
-//         }
-//     }
 }
