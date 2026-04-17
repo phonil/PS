de1861822689215499fe8c874094fa10cea83ed9
강승우 <112797234+phonil@users.noreply.github.com>
2026-04-17 15:54:07 +0900

    [Silver IV] Title: 수 찾기, Time: 628 ms, Memory: 47428 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,33 +5,42 @@ import java.util.Arrays;
 
 public class Main {
 
-    static int[] arr;
+    static int[] A;
     static int N, M;
-    static int binarySearch(int target) {
+
+    static boolean binarySearch(int target) {
         int st = 0;
         int en = N - 1;
         while (st <= en) {
             int mid = (st + en) / 2;
-            if (target < arr[mid]) en = mid - 1;
-            else if (target > arr[mid]) st = mid + 1;
-            else return 1;
+            if (A[mid] > target)
+                en = mid - 1;
+            else if (A[mid] < target)
+                st = mid + 1;
+            else
+                return true;
         }
-        return 0;
+        return false;
     }
 
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        A = new int[N];
         String[] split = br.readLine().split(" ");
-        arr = new int[N];
         for (int i = 0; i < N; i++)
-            arr[i] = Integer.parseInt(split[i]);
-        Arrays.sort(arr);
+            A[i] = Integer.parseInt(split[i]);
+
+        Arrays.sort(A);
         M = Integer.parseInt(br.readLine());
         split = br.readLine().split(" ");
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(split[i]);
-            System.out.println(binarySearch(target));
+            if (!binarySearch(target)) sb.append(0);
+            else sb.append(1);
+            sb.append('\n');
         }
+        System.out.print(sb);
     }
 }
